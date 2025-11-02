71521430f2a7d9b69c5032692f2faaed28062953
Lucian Parvu <lucian@erlang.ro>
2025-11-02 18:40:02 +0200

    initial commit - empty mix project

---
+++ lib/pcap_file_ex.ex
@@ -0,0 +1,18 @@
+defmodule PcapFileEx do
+  @moduledoc """
+  Documentation for `PcapFileEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PcapFileEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pcap_file_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule PcapFileExTest do
+  use ExUnit.Case
+  doctest PcapFileEx
+
+  test "greets the world" do
+    assert PcapFileEx.hello() == :world
+  end
+end
