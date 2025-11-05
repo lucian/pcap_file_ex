adc28eec3c64b924332a7464bb41b7a9f37b0f5d
Lucian Parvu <lucian@erlang.ro>
2025-11-05 17:09:25 +0200

    fixing build warnings

--- native/pcap_file_ex/src/lib.rs
+++ native/pcap_file_ex/src/lib.rs
@@ -5,10 +5,10 @@ mod types;
 
 use rustler::{Env, Term};
 
+#[allow(non_local_definitions)]
 fn load(env: Env, _info: Term) -> bool {
-    rustler::resource!(pcap::PcapReaderResource, env);
-    rustler::resource!(pcapng::PcapNgReaderResource, env);
-    true
+    rustler::resource!(pcap::PcapReaderResource, env)
+        && rustler::resource!(pcapng::PcapNgReaderResource, env)
 }
 
 rustler::init!("Elixir.PcapFileEx.Native", load = load);
