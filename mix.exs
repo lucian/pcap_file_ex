72495fa306626ebde08a13a48f2737a25fc46b04
Lucian Parvu <lucian@erlang.ro>
2026-01-07 23:21:56 +0200

    Prepare v0.5.7 for HEX publication

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PcapFileEx.MixProject do
   use Mix.Project
 
-  @version "0.5.7-dev"
+  @version "0.5.7"
   @source_url "https://github.com/lucian/pcap_file_ex"
   @dev? String.ends_with?(@version, "-dev")
   @force_build? System.get_env("PCAP_FILE_EX_BUILD") in ["1", "true"]
